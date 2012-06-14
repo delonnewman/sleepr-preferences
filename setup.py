050a2c54f03cbbf55794780afdac43689ccbbaab
Delon Newman <delon.newman@gmail.com>
2012-06-13 21:30:01 -1000

    working on setup script

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Sleepr Preferences',
+      version='1.0',
+      description='Stop hacking and get some sleep',
+      author='Delon Newman',
+      author_email='delon.newman@gmail.com',
+      url='https://github.com/delonnewman/sleepr-preferences',
+      packages=['sleepr-preferences'],
+     )
